5b182a35a1eba8bcba69fe39f535d9400db1db52
David Mytton <david@davidmytton.co.uk>
2020-09-14 09:59:49 +0100

    Remove nmap because it proved unable to discover anything useful

--- analyse.py
+++ analyse.py
@@ -1,8 +1,14 @@
 # Traceroute Collector
 #
-# This code packages the methodology for my Environmental Technology MSc
-# thesis at Imperial College London so that the required traceroute data
-# can be collected from volunteers as easily as possible.
+# This analysis script aggregates all the results files into a single CSV.
+# The results files are expected to use following naming convention:
+#
+# results-COUNTRY-CITY-DESTINATION-CONNECTION.json
+# e.g. results-uk-london-www.instagram.com-4g.json
+#
+# Place all the results files into a single directory and then run:
+#
+# python3 analyse.py --results_dir RESULTS_DIR --ipinfo_key API_KEY
 #
 # Copyright (C) 2020 David Mytton <david@davidmytton.co.uk>
 # This program is free software; you can redistribute it and/or modify
@@ -22,17 +28,11 @@
 # System
 import argparse
 import csv
-import ipaddress
 import json
 import os
 
 # 3rd party
 import ipinfo
-import nmap
-
-# nmap needs root
-if os.geteuid() != 0:
-    exit('You must run this with sudo')
 
 # Parse args
 parser = argparse.ArgumentParser()
@@ -42,13 +42,7 @@ parser.add_argument('--results_dir',
 parser.add_argument('--ipinfo_key',
                     help='API key for ipinfo.io',
                     required=True)
-parser.add_argument('--ipv',
-                    help='Which version of IP to test from. Specify 4 or 6',
-                    required=True)
 args = parser.parse_args()
-
-if args.ipv != '4' and args.ipv != '6':
-    exit('Incorrect value for --ipv, must be 4 or 6')
 
 fieldnames = ['Participant ID',
               'Participant City',
@@ -63,25 +57,44 @@ fieldnames = ['Participant ID',
               'Destination City',
               'Destination Country',
               'Trace Hop Count',
-              'Hop 1 IP', 'Hop 1 Hostname', 'Hop 1 OS (nmap name)', 'Hop 1 OS (nmap confidence)', 'Hop 1 RTT', 'Hop 1 ASN', 'Hop 1 City', 'Hop 1 Country',
-              'Hop 2 IP', 'Hop 2 Hostname', 'Hop 2 OS (nmap name)', 'Hop 2 OS (nmap confidence)', 'Hop 2 RTT', 'Hop 2 ASN', 'Hop 2 City', 'Hop 2 Country',
-              'Hop 3 IP', 'Hop 3 Hostname', 'Hop 3 OS (nmap name)', 'Hop 3 OS (nmap confidence)', 'Hop 3 RTT', 'Hop 3 ASN', 'Hop 3 City', 'Hop 3 Country',
-              'Hop 4 IP', 'Hop 4 Hostname', 'Hop 4 OS (nmap name)', 'Hop 4 OS (nmap confidence)', 'Hop 4 RTT', 'Hop 4 ASN', 'Hop 4 City', 'Hop 4 Country',
-              'Hop 5 IP', 'Hop 5 Hostname', 'Hop 5 OS (nmap name)', 'Hop 5 OS (nmap confidence)', 'Hop 5 RTT', 'Hop 5 ASN', 'Hop 5 City', 'Hop 5 Country',
-              'Hop 6 IP', 'Hop 6 Hostname', 'Hop 6 OS (nmap name)', 'Hop 6 OS (nmap confidence)', 'Hop 6 RTT', 'Hop 6 ASN', 'Hop 6 City', 'Hop 6 Country',
-              'Hop 7 IP', 'Hop 7 Hostname', 'Hop 7 OS (nmap name)', 'Hop 7 OS (nmap confidence)', 'Hop 7 RTT', 'Hop 7 ASN', 'Hop 7 City', 'Hop 7 Country',
-              'Hop 8 IP', 'Hop 8 Hostname', 'Hop 8 OS (nmap name)', 'Hop 8 OS (nmap confidence)', 'Hop 8 RTT', 'Hop 8 ASN', 'Hop 8 City', 'Hop 8 Country',
-              'Hop 9 IP', 'Hop 9 Hostname', 'Hop 9 OS (nmap name)', 'Hop 9 OS (nmap confidence)', 'Hop 9 RTT', 'Hop 9 ASN', 'Hop 9 City', 'Hop 9 Country',
-              'Hop 10 IP', 'Hop 10 Hostname', 'Hop 10 OS (nmap name)', 'Hop 10 OS (nmap confidence)', 'Hop 10 RTT', 'Hop 10 ASN', 'Hop 10 City', 'Hop 10 Country',
-              'Hop 11 IP', 'Hop 11 Hostname', 'Hop 11 OS (nmap name)', 'Hop 11 OS (nmap confidence)', 'Hop 11 RTT', 'Hop 11 ASN', 'Hop 11 City', 'Hop 11 Country',
-              'Hop 12 IP', 'Hop 12 Hostname', 'Hop 12 OS (nmap name)', 'Hop 12 OS (nmap confidence)', 'Hop 12 RTT', 'Hop 12 ASN', 'Hop 12 City', 'Hop 12 Country',
-              'Hop 13 IP', 'Hop 13 Hostname', 'Hop 13 OS (nmap name)', 'Hop 13 OS (nmap confidence)', 'Hop 13 RTT', 'Hop 13 ASN', 'Hop 13 City', 'Hop 13 Country',
-              'Hop 14 IP', 'Hop 14 Hostname', 'Hop 14 OS (nmap name)', 'Hop 14 OS (nmap confidence)', 'Hop 14 RTT', 'Hop 14 ASN', 'Hop 14 City', 'Hop 14 Country',
-              'Hop 15 IP', 'Hop 15 Hostname', 'Hop 15 OS (nmap name)', 'Hop 15 OS (nmap confidence)', 'Hop 15 RTT', 'Hop 15 ASN', 'Hop 15 City', 'Hop 15 Country',
-              'Hop 16 IP', 'Hop 16 Hostname', 'Hop 16 OS (nmap name)', 'Hop 16 OS (nmap confidence)', 'Hop 16 RTT', 'Hop 16 ASN', 'Hop 16 City', 'Hop 16 Country',
-              'Hop 17 IP', 'Hop 17 Hostname', 'Hop 17 OS (nmap name)', 'Hop 17 OS (nmap confidence)', 'Hop 17 RTT', 'Hop 17 ASN', 'Hop 17 City', 'Hop 17 Country',
-              'Hop 18 IP', 'Hop 18 Hostname', 'Hop 18 OS (nmap name)', 'Hop 18 OS (nmap confidence)', 'Hop 18 RTT', 'Hop 18 ASN', 'Hop 18 City', 'Hop 18 Country',
-              'Hop 19 IP', 'Hop 19 Hostname', 'Hop 19 OS (nmap name)', 'Hop 19 OS (nmap confidence)', 'Hop 19 RTT', 'Hop 19 ASN', 'Hop 19 City', 'Hop 19 Country']
+              'Hop 1 IP', 'Hop 1 Hostname', 'Hop 1 RTT', 'Hop 1 ASN',
+              'Hop 1 City', 'Hop 1 Country',
+              'Hop 2 IP', 'Hop 2 Hostname', 'Hop 2 RTT', 'Hop 2 ASN',
+              'Hop 2 City', 'Hop 2 Country',
+              'Hop 3 IP', 'Hop 3 Hostname', 'Hop 3 RTT', 'Hop 3 ASN',
+              'Hop 3 City', 'Hop 3 Country',
+              'Hop 4 IP', 'Hop 4 Hostname', 'Hop 4 RTT', 'Hop 4 ASN',
+              'Hop 4 City', 'Hop 4 Country',
+              'Hop 5 IP', 'Hop 5 Hostname', 'Hop 5 RTT', 'Hop 5 ASN',
+              'Hop 5 City', 'Hop 5 Country',
+              'Hop 6 IP', 'Hop 6 Hostname', 'Hop 6 RTT', 'Hop 6 ASN',
+              'Hop 6 City', 'Hop 6 Country',
+              'Hop 7 IP', 'Hop 7 Hostname', 'Hop 7 RTT', 'Hop 7 ASN',
+              'Hop 7 City', 'Hop 7 Country',
+              'Hop 8 IP', 'Hop 8 Hostname', 'Hop 8 RTT', 'Hop 8 ASN',
+              'Hop 8 City', 'Hop 8 Country',
+              'Hop 9 IP', 'Hop 9 Hostname', 'Hop 9 RTT', 'Hop 9 ASN',
+              'Hop 9 City', 'Hop 9 Country',
+              'Hop 10 IP', 'Hop 10 Hostname', 'Hop 10 RTT', 'Hop 10 ASN',
+              'Hop 10 City', 'Hop 10 Country',
+              'Hop 11 IP', 'Hop 11 Hostname', 'Hop 11 RTT', 'Hop 11 ASN',
+              'Hop 11 City', 'Hop 11 Country',
+              'Hop 12 IP', 'Hop 12 Hostname', 'Hop 12 RTT', 'Hop 12 ASN',
+              'Hop 12 City', 'Hop 12 Country',
+              'Hop 13 IP', 'Hop 13 Hostname', 'Hop 13 RTT', 'Hop 13 ASN',
+              'Hop 13 City', 'Hop 13 Country',
+              'Hop 14 IP', 'Hop 14 Hostname', 'Hop 14 RTT', 'Hop 14 ASN',
+              'Hop 14 City', 'Hop 14 Country',
+              'Hop 15 IP', 'Hop 15 Hostname', 'Hop 15 RTT', 'Hop 15 ASN',
+              'Hop 15 City', 'Hop 15 Country',
+              'Hop 16 IP', 'Hop 16 Hostname', 'Hop 16 RTT', 'Hop 16 ASN',
+              'Hop 16 City', 'Hop 16 Country',
+              'Hop 17 IP', 'Hop 17 Hostname', 'Hop 17 RTT', 'Hop 17 ASN',
+              'Hop 17 City', 'Hop 17 Country',
+              'Hop 18 IP', 'Hop 18 Hostname', 'Hop 18 RTT', 'Hop 18 ASN',
+              'Hop 18 City', 'Hop 18 Country',
+              'Hop 19 IP', 'Hop 19 Hostname', 'Hop 19 RTT', 'Hop 19 ASN',
+              'Hop 19 City', 'Hop 19 Country']
 
 with open('results.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -100,14 +113,16 @@ with open('results.csv', 'w') as csvfile:
                 print('- filesplit...')
 
                 # Parse details from filename
-                scamper_filename = scamper_filename.replace(args.results_dir, '')  # Remove directory name
-                scamper_filename_split = scamper_filename.split('-')  # Separate components
-                # e.g. ['results', 'uk', 'london', '15', 'www.instagram.com', '4g.json']
+                scamper_filename = scamper_filename.replace(
+                    args.results_dir, '')  # Remove directory name
+                # Separate components
+                scamper_filename_split = scamper_filename.split('-')
 
                 csv_line['Participant ID'] = scamper_filename_split[3]
                 csv_line['Participant City'] = scamper_filename_split[2]
                 csv_line['Participant Country'] = scamper_filename_split[1]
-                csv_line['Connection'] = scamper_filename_split[5].replace('.json', '')
+                csv_line['Connection'] = scamper_filename_split[5].replace(
+                    '.json', '')
                 csv_line['IPv'] = args.ipv
                 csv_line['Destination'] = scamper_filename_split[4]
                 csv_line['Destination IP'] = trace['dst']
@@ -132,8 +147,9 @@ with open('results.csv', 'w') as csvfile:
                 # How many hops?
                 hops = trace['hop_count']
 
+                # If we hit the gap limit (5), subtract from the total
                 if trace['stop_reason'] == 'GAPLIMIT':
-                    hops = hops - 5  # If we hit the gap limit (5), subtract from the total
+                    hops = hops - 5
 
                 csv_line['Trace Hop Count'] = hops
 
@@ -141,45 +157,42 @@ with open('results.csv', 'w') as csvfile:
 
                 # Loop through the hops. For each hop:
                 # - query the IP from https://ipinfo.io
-                # - run nmap OS detection if it's not a local IP
                 if 'hops' in trace:
                     for hop in trace['hops']:
                         print('-- %s - ipinfo...' % (hop['addr']))
                         ip_details = ipinfo_handler.getDetails(hop['addr'])
                         hop_number = hop['probe_ttl']
 
-                        csv_line['Hop ' + str(hop_number) + ' IP'] = hop['addr']
-                        csv_line['Hop ' + str(hop_number) + ' RTT'] = hop['rtt']
+                        csv_line['Hop '
+                                 + str(hop_number)
+                                 + ' IP'] = hop['addr']
+                        csv_line['Hop '
+                                 + str(hop_number)
+                                 + ' RTT'] = hop['rtt']
 
                         if hasattr(ip_details, 'hostname'):
-                            csv_line['Hop ' + str(hop_number) + ' Hostname'] = ip_details.hostname
+                            csv_line['Hop '
+                                     + str(hop_number)
+                                     + ' Hostname'] = ip_details.hostname
                         else:
-                            csv_line['Hop ' + str(hop_number) + ' Hostname'] = 'Unknown'
+                            csv_line['Hop '
+                                     + str(hop_number)
+                                     + ' Hostname'] = 'Unknown'
 
                         if not hasattr(ip_details, 'bogon'):
                             if hasattr(ip_details, 'org'):
-                                csv_line['Hop ' + str(hop_number) + ' ASN'] = ip_details.org
+                                csv_line['Hop '
+                                         + str(hop_number)
+                                         + ' ASN'] = ip_details.org
                             if hasattr(ip_details, 'city'):
-                                csv_line['Hop ' + str(hop_number) + ' City'] = ip_details.city
+                                csv_line['Hop '
+                                         + str(hop_number)
+                                         + ' City'] = ip_details.city
                             if hasattr(ip_details, 'country'):
-                                csv_line['Hop ' + str(hop_number) + ' Country'] = ip_details.country_name
-
-                            print('-- %s - nmap...' % (hop['addr']))
-
-                            nm = nmap.PortScanner()
-
-                            if args.ipv == '4':
-                                nmap_arguments = '-O'
-                            elif args.ipv == '6':
-                                nmap_arguments = '-O -6'
-
-                            nm.scan(hop['addr'], arguments=nmap_arguments)
-                            try:
-                                if 'osmatch' in nm[hop['addr']]:
-                                    csv_line['Hop ' + str(hop_number) + ' OS (nmap name)'] = nm[hop['addr']]['osmatch'][0]['name']
-                                    csv_line['Hop ' + str(hop_number) + ' OS (nmap confidence)'] = nm[hop['addr']]['osmatch'][0]['accuracy']
-                            except KeyError:
-                                pass
+                                csv_line['Hop '
+                                         + str(hop_number)
+                                         + ' Country'] \
+                                         = ip_details.country_name
 
                 writer.writerow(csv_line)
 
